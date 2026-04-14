20a174229324ca066e6022772806d1d1795a6f78
arya <aryasomu18@gmail.com>
2026-04-14 16:57:57 -0700

    feat: optimistic-ZK proving, challenge window, fast-finality path (VEL-16)

--- zkvm/src/lib.rs
+++ zkvm/src/lib.rs
@@ -1,2 +1,7 @@
 pub mod prover;
-pub use prover::{ZkvmInput, ZkvmOutput, execute_stf};
+pub mod optimistic;
+
+pub use prover::{ZkvmInput, ZkvmOutput, execute_stf, verify_execution};
+pub use optimistic::{
+    ChallengeStatus, OptimisticProver, ProofStatus, CHALLENGE_WINDOW_SECS,
+};
